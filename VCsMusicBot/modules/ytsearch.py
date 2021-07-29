5cc6354ad5378c3955289f14e2d651699bb0c927
@ASHIRU MALSHAN <79177943+ASHIRUMALSHAN@users.noreply.github.com>
2021-07-29 12:45:27 +0530

    Update ytsearch.py

--- VCsMusicBot/modules/ytsearch.py
+++ VCsMusicBot/modules/ytsearch.py
@@ -15,7 +15,7 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@app.on_message(pyrogram.filters.command(["ytsearch"]))
+@app.on_message(pyrogram.filters.command(["search" ,"ytsearch"]))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
@@ -27,11 +27,11 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 5:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
-            text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"- 🎵 Title : {results[i]['title']}\n"
+            text += f"- ⏳ Duration : {results[i]['duration']}\n"
+            text += f"- 👁 Views : {results[i]['views']}\n"
+            text += f"- 📺 Channel : {results[i]['channel']}\n"
+            text += f"- 🔗 Link : https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
