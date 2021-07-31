03ee23fe1274090cfe6138b658fb43d3f765bcdf
@ASHIRU MALSHAN <79177943+ASHIRUMALSHAN@users.noreply.github.com>
2021-07-31 20:01:13 +0530

    Update ytsearch.py

--- VCsMusicBot/modules/ytsearch.py
+++ VCsMusicBot/modules/ytsearch.py
@@ -16,6 +16,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 @app.on_message(pyrogram.filters.command(["search" ,"ytsearch"]))
+if query.data == "yt_search"
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
