8970f7539e977bf331840f1e21f9a4b42d847f33
@ASHIRU MALSHAN <79177943+ASHIRUMALSHAN@users.noreply.github.com>
2021-07-31 12:13:55 +0530

    Update ytsearch.py

--- VCsMusicBot/modules/ytsearch.py
+++ VCsMusicBot/modules/ytsearch.py
@@ -27,11 +27,11 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 5:
-            text += f"- 🎵 Title : {results[i]['title']}\n"
-            text += f"- ⏳ Duration : {results[i]['duration']}\n"
-            text += f"- 👁 Views : {results[i]['views']}\n"
-            text += f"- 📺 Channel : {results[i]['channel']}\n"
-            text += f"- 🔗 Link : https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"**- 🎵 Title :** {results[i]['title']}\n"
+            text += f"**- ⏳ Duration :** {results[i]['duration']}\n"
+            text += f"**- 👁 Views :** {results[i]['views']}\n"
+            text += f"**- 📺 Channel :** {results[i]['channel']}\n"
+            text += f"**- 🔗 Link :** https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
